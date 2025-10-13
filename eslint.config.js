9c76dcc9a6a0066406e0c2407f04c86dc007d605
hasibprangon <hasibulhossainp09@gmail.com>
2025-10-14 00:55:45 +0600

    route setup done

--- eslint.config.js
+++ eslint.config.js
@@ -25,5 +25,7 @@ export default defineConfig([
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
+    'react/prop-types': 'off',
+      'no-unused-vars': 'off'
   },
 ])
